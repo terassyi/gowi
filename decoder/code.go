d40900f251025c940fb83757fb15dc8df8e53aac
terassyi <iscale821@gmail.com>
2022-03-24 12:01:27 +0900

    fix decode code for block

--- decoder/code.go
+++ decoder/code.go
@@ -45,30 +45,35 @@ func newCode(payload []byte) (*code, error) {
 }
 
 func newFunctionBody(buf *bytes.Buffer) (*functionBody, error) {
-	_, _, err := types.DecodeVarUint32(buf)
+	size, _, err := types.DecodeVarUint32(buf)
 	if err != nil {
 		return nil, fmt.Errorf("newFunctionBody: decode body_size: %w", err)
 	}
-	count, _, err := types.DecodeVarUint32(buf)
+	data := make([]byte, int(size))
+	if _, err := buf.Read(data); err != nil {
+		return nil, fmt.Errorf("newFunctionBody: decode: %w", err)
+	}
+	bodyBuf := bytes.NewBuffer(data)
+	count, _, err := types.DecodeVarUint32(bodyBuf)
 	if err != nil {
 		return nil, fmt.Errorf("newFunctionBody: decode local_count: %w", err)
 	}
 	locals := make([]*localEntry, 0, int(count))
 	for i := 0; i < int(count); i++ {
-		l, err := newLocalEntry(buf)
+		l, err := newLocalEntry(bodyBuf)
 		if err != nil {
 			return nil, fmt.Errorf("newFunctionBody: decode locals: %w", err)
 		}
 		locals = append(locals, l)
 	}
-	code, err := buf.ReadBytes(END)
-	if err != nil {
-		return nil, fmt.Errorf("newFunctionBody: decode code: %w", err)
-	}
-	codeBuf := bytes.NewBuffer(code)
+	// code, err := buf.ReadBytes(END)
+	// if err != nil {
+	// 	return nil, fmt.Errorf("newFunctionBody: decode code: %w", err)
+	// }
+	// codeBuf := bytes.NewBuffer(code)
 	codes := make([]instruction.Instruction, 0, 1024)
-	for codeBuf.Len() > 0 {
-		c, err := instruction.Decode(codeBuf)
+	for bodyBuf.Len() > 0 {
+		c, err := instruction.Decode(bodyBuf)
 		if err != nil {
 			return nil, fmt.Errorf("newFunctionBody: decode instruction: %w", err)
 		}
---
+++ examples/if.wat
@@ -0,0 +1,5 @@
+(module
+	(func (export "if_func")
+		(if (i32.const 0) (then (nop)))
+	)
+)
